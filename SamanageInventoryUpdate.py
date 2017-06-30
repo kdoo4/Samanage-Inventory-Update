90f48488645a8fc6dca86961c420abbed31c5f15
Kielan Doo <kdoo@las.ch>
2017-06-30 11:41:45 +0200

    Add conditions for update and add asset, created the update to Samanage

--- SamanageInventoryUpdate.py
+++ SamanageInventoryUpdate.py
@@ -5,15 +5,57 @@ import json
 import csv
 import os
 
+#Call Samanage credentials from the environment
+"""
 samanage_username = str(os.getenv('SAMANAGE_USERNAME'))
 samanage_key = str(os.getenv('SAMANAGE_KEY'))
-
+"""
 if  samanage_username == None or samanage_key == None:
     print "Please setup SAMANAGE_KEY and SAMANAGE_USERNAME variables in your environement"
     quit()
 
 url = 'https://apieu.samanage.com'
-request = '/incidents.xml'
 
-connect = requests.get(url+request, auth = (samanage_username, samanage_key))
-print connect.text
+request = requests.get(url, auth = (samanage_username, samanage_key))
+print request.status_code
+
+print "Enter your file path below"
+filepath = raw_input()
+
+with open(file, 'rb') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader, None)
+    rownumber = 2
+
+    for row in reader:
+        if row[11] == 'P' or 'p':
+            pass
+
+        else if row[11] == 'M' or 'm':
+            extension = '/other_assets/%s.xml' %(row[10])
+            for entry in row:
+                if entry[0] == '@':
+                    content == entry[1:]
+                    if row.index(entry) == 6:
+                        field == 'Site'
+                    else if row.index(entry) == 7:
+                        field == 'Room'
+                    else if row.index(entry) == 8:
+                        field == 'Department'
+                    else if row.index(entry) == 9:
+                        field == 'User'
+                    else:
+                        continue
+                    data = {"other_asset":{ "%s":"%s" }, } %(field, content)
+                    data = json.dumps(data)
+                    #request = requests.post(url+extension, auth=(samanage_username, samanage_key), data=json.dumps(data))
+
+        else if row[11] == 'N' or 'n':
+            extension = '/other_assets.xml'
+            pass
+        else:
+            print "Error: Incomplete inventory status column, row %d. Row skipped." %(rownumber)
+            next
+
+
+        rownumber += 1
--- csvtest.py
+++ csvtest.py
@@ -6,3 +6,10 @@ with open("csv.csv", "rb") as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
         print row[1]
+
+'''
+word = "word"
+print word
+newword = word[1:]
+print newword
+'''
